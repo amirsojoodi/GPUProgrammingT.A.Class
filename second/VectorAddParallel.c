3eb3c69692fca37712d24cfdc5e10f5fe6088140
AmirHossein Sojoodi <sojoodi@cse.shirazu.ac.ir>
2015-10-12 01:37:12 +0330

    complete naive vectorAddParallel

--- second/VectorAddParallel.c
+++ second/VectorAddParallel.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<pthread.h>
 #include<time.h>
 #include<sys/time.h>
 
-void sequential_naive_vectorAdd(int *a, int *b, int *c, int size){
-    for(int i = 0; i < size; i++){
-        c[i] = a[i] + b[i];
-    }
-}
+#define min(a,b) ((a) < (b) ? a : b)
 
 typedef struct argument{
 	int *a;
@@ -17,6 +14,34 @@ typedef struct argument{
 	int size;
 	int num_thread;
 } argument;
+
+void validate(int *a, int *b, int length) {
+	for (int i = 0; i < length; ++i) {
+		if (a[i] != b[i]) {
+			printf("Different value detected at position: %d,"
+					"expected %d but get %d\n", i, a[i], b[i]);
+			break;
+		}
+	}
+}
+
+void *naive_parallel_vector_add(void *arg){
+	argument targ = *(argument *)arg;
+
+	int si = (targ.size / targ.num_thread) * targ.tid; 
+	int ei = min(si + targ.size / targ.num_thread, targ.size);
+
+	for(int i = si; i < ei; i++){
+		targ.c[i] = targ.a[i] + targ.b[i];
+	}
+}
+
+void sequential_naive_vectorAdd(int *a, int *b, int *c, int size){
+    for(int i = 0; i < size; i++){
+        c[i] = a[i] + b[i];
+    }
+}
+
 
 // vectorAddParallel Input_size thread
 int main(int argc, char *argv[]){
@@ -31,16 +56,18 @@ int main(int argc, char *argv[]){
 	
 	pthread_t threads[num_thread];
 	argument arguments[num_thread];
-
-	int *a, *b, *c;
+	struct timeval start, end;
+	int *a, *b, *c, *d;
 	a = (int *)malloc(sizeof(int) * size);
 	b = (int *)malloc(sizeof(int) * size);
 	c = (int *)malloc(sizeof(int) * size);
+	d = (int *)malloc(sizeof(int) * size);
 
 	for(int i = 0; i <size; i++){
 		a[i] = rand() % 100;
 		b[i] = rand() % 100;
 		c[i] = 0;
+		d[i] = 0;
 	}
 
 	gettimeofday(&start, NULL);
@@ -49,16 +76,37 @@ int main(int argc, char *argv[]){
 
     gettimeofday(&end, NULL);
 
-    double diff = (end.tv_sec - start.tv_sec) * 1000.0 +
+    double diff = (end.tv_sec - start.tv_sec) * 1000000.0 +
         (end.tv_usec - start.tv_usec);
 
-    printf("Naive VectorAdd time calculation duration: %8.5fms\n", diff);
+    printf("Naive VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
+
+    gettimeofday(&start, NULL);
+
+	for(int i = 0; i < num_thread; i++){
+		arguments[i].a = a;
+		arguments[i].b = b;
+		arguments[i].c = d;
+		arguments[i].tid = i;
+		arguments[i].size = size;
+		arguments[i].num_thread = num_thread;
+	
+		pthread_create(&threads[i], 0, naive_parallel_vector_add, (void *)&arguments[i]);
+	}
+	
+	int status;
+    for (int i = 0; i < num_thread; ++i) {
+        pthread_join(threads[i], (void *)&status);
+    }
 
 
+	gettimeofday(&end, NULL);
 
+    diff = (end.tv_sec - start.tv_sec) * 1000000.0
+            + (end.tv_usec - start.tv_usec);
+    printf("Parallel VectorAdd time calculation duration: %8.4fms\n", diff / 1000);
 
-
-
+	validate(c, d, size);
 
 	return 0;
 }

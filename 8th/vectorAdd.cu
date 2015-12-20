301be80fb8e5f89d6cf6a39a00e290e972c6b28e
AmirHossein Sojoodi <sojoodi@cse.shirazu.ac.ir>
2015-12-20 17:09:18 +0330

    updates 8th session

--- 8th/vectorAdd.cu
+++ 8th/vectorAdd.cu
@@ -23,23 +23,23 @@ void initialize(int *a, int *b, int *c, int *d, int input_length) {
 }
 
 void validate(int *a, int *b, int length) {
-    for (int i = 0; i < length; ++i) {
-        if (a[i] != b[i]) {
-            printf("Different value detected at position: %d,"
-                    "expected %d but get %d\n", i, a[i], b[i]);
-            break;
-        }
-    }
+	for (int i = 0; i < length; ++i) {
+		if (a[i] != b[i]) {
+			printf("Different value detected at position: %d,"
+					"expected %d but get %d\n", i, a[i], b[i]);
+			break;
+		}
+	}
 }
 
 void vector_add(int *a, int *b, int *c, int size) {
-    for (int i = 0; i < size; i++) {
-        c[i] = a[i] + b[i];
-    }
+	for (int i = 0; i < size; i++) {
+		c[i] = a[i] + b[i];
+	}
 }
 
 __global__ void vector_add_kernel_coalesced_access(int *a_d, int *b_d, int *d_d, int work_per_thread, int input_length, int totalThreads) {
-    
+
 	int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
 	for(int i = 0; i < work_per_thread && tid < input_length; i++, tid += totalThreads){
@@ -50,31 +50,31 @@ __global__ void vector_add_kernel_coalesced_access(int *a_d, int *b_d, int *d_d,
 
 int main(int argc, char *argv[]) {
 
-    int input_length, block_size, work_per_thread;
+	int input_length, block_size, work_per_thread;
 	struct timeval start, end;
 
-    if (argc != 2) {
-        printf("Usage is: VectorAddParallel input_length block_size work_per_thread\nNow, type input_length: ");
-        scanf("%d", &input_length);
+	if (argc != 2) {
+		printf("Usage is: VectorAddParallel input_length block_size work_per_thread\nNow, type input_length: ");
+		scanf("%d", &input_length);
 		printf("Type block_size: ");
 		scanf("%d", &block_size);
 		printf("Type work_per_thread: ");
 		scanf("%d", &work_per_thread);
-    } else{
-        input_length = atoi(argv[1]);
+	} else{
+		input_length = atoi(argv[1]);
 		block_size = atoi(argv[2]);
 		work_per_thread = atoi(argv[3]);
-    }
+	}
 
 	// Arrays declaration
-    int *a_h, *b_h, *c_h, *d_h;
+	int *a_h, *b_h, *c_h, *d_h;
 	int *a_d, *b_d, *d_d;
 
 	// Allocation on Host
-    a_h = (int *) malloc(sizeof(int) * input_length);
-    b_h = (int *) malloc(sizeof(int) * input_length);
-    c_h = (int *) malloc(sizeof(int) * input_length);
-    d_h = (int *) malloc(sizeof(int) * input_length);
+	a_h = (int *) malloc(sizeof(int) * input_length);
+	b_h = (int *) malloc(sizeof(int) * input_length);
+	c_h = (int *) malloc(sizeof(int) * input_length);
+	d_h = (int *) malloc(sizeof(int) * input_length);
 
 	// Allocation on Device
 	CUDA_CHECK_RETURN(cudaMalloc((void **)&a_d, sizeof(int)*input_length));
@@ -82,7 +82,7 @@ int main(int argc, char *argv[]) {
 	CUDA_CHECK_RETURN(cudaMalloc((void **)&d_d, sizeof(int)*input_length));
 
 	// Initialization on host side
-    initialize(a_h, b_h, c_h, d_h, input_length);
+	initialize(a_h, b_h, c_h, d_h, input_length);
 
 	// Copy input data to device
 	CUDA_CHECK_RETURN(cudaMemcpy(a_d, a_h, sizeof(int)*input_length, cudaMemcpyHostToDevice));
@@ -92,29 +92,36 @@ int main(int argc, char *argv[]) {
 	gettimeofday(&start, NULL);
 	vector_add(a_h, b_h, c_h, input_length);
 	gettimeofday(&end, NULL);
-	
-	double diff = (end.tv_sec - start.tv_sec) * 1000000.0 +
-        (end.tv_usec - start.tv_usec);
 
-    printf("Host VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
+	double diff = (end.tv_sec - start.tv_sec) * 1000000.0 +
+		(end.tv_usec - start.tv_usec);
+
+	printf("Host VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
 
 	// Run device code
 	int grid_size = (input_length - 1) / (block_size * work_per_thread) + 1;
-		
+
 	dim3 grid_dime(grid_size, 1, 1);
 	dim3 block_dime(block_size, 1, 1);
 	int totalThreads = grid_size * block_size;
 
 	gettimeofday(&start, NULL);
+
+	for (int i = 0; i < nStreams; ++i) {
+		int offset = i * streamSize;
+		cudaMemcpyAsync(&d_a[offset], &a[offset], streamBytes, cudaMemcpyHostToDevice, stream[i]);
+		kernel<<<streamSize/blockSize, blockSize, 0, stream[i]>>>(d_a, offset);
+		cudaMemcpyAsync(&a[offset], &d_a[offset], streamBytes, cudaMemcpyDeviceToHost, stream[i]);
+	}
 	vector_add_kernel_coalesced_access<<< grid_dime, block_dime >>>(a_d, b_d, d_d, work_per_thread, input_length, totalThreads);
 	CUDA_CHECK_RETURN(cudaDeviceSynchronize());	// Wait for the GPU launched work to complete
 	CUDA_CHECK_RETURN(cudaGetLastError());
 	gettimeofday(&end, NULL);
 
 	diff = (end.tv_sec - start.tv_sec) * 1000000.0 +
-        (end.tv_usec - start.tv_usec);
-        
-    printf("Device VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
+		(end.tv_usec - start.tv_usec);
+
+	printf("Device VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
 
 	//Copy back the result
 	CUDA_CHECK_RETURN(cudaMemcpy(d_h, d_d, sizeof(int)*input_length, cudaMemcpyDeviceToHost));
